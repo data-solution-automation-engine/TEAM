adcf3a0e50f3475ca24d635862fc6d6b427917cb
Luciano Bastian Machado <lucianobastianmachado@yahoo.com>
2019-05-28 10:10:01 +1000

    Fix BUG - When LNK BK is different of LSAT BK (e.g contains spaces)

--- TEAM/Form_Manage_Metadata.cs
+++ TEAM/Form_Manage_Metadata.cs
@@ -5242,10 +5242,7 @@ namespace TEAM
                     prepareMappingStatement.AppendLine("JOIN ALL_DATABASE_COLUMNS ADC_TARGET ON XREF.TARGET_SCHEMA_NAME = ADC_TARGET.[SCHEMA_NAME] AND XREF.TARGET_NAME = ADC_TARGET.TABLE_NAME");
                     prepareMappingStatement.AppendLine("LEFT OUTER JOIN dbo.MD_ATTRIBUTE stg_attr ON ADC_SOURCE.COLUMN_NAME = stg_attr.ATTRIBUTE_NAME COLLATE DATABASE_DEFAULT");
                     prepareMappingStatement.AppendLine("LEFT OUTER JOIN dbo.MD_ATTRIBUTE tgt_attr ON ADC_TARGET.COLUMN_NAME = tgt_attr.ATTRIBUTE_NAME COLLATE DATABASE_DEFAULT");
-                    prepareMappingStatement.AppendLine("WHERE stg_attr.ATTRIBUTE_ID = tgt_attr.ATTRIBUTE_ID");
-
-                    File.AppendAllText(@"C:\temp\SQL.txt", prepareMappingStatement.ToString());
-                    
+                    prepareMappingStatement.AppendLine("WHERE stg_attr.ATTRIBUTE_ID = tgt_attr.ATTRIBUTE_ID");                    
                     
                     var automaticAttributeMappings = GetDataTable(ref connOmd, prepareMappingStatement.ToString());
 
