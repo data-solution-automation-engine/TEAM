6d0dd75507a659a133f4e9f7e08de1be580f2b4e
Roelant Vos <roelant@ravos.nl>
2022-11-15 20:35:30 +1000

    Update SortableBindingList.cs

--- Team_Library/SortableBindingList.cs
+++ Team_Library/SortableBindingList.cs
@@ -21,8 +21,7 @@ namespace TEAM_Library
         /// Initializes a new instance of the <see cref="SortableBindingList{T}"/> class.
         /// </summary>
         /// <param name="list">An <see cref="T:System.Collections.Generic.IList`1" /> of items to be contained in the <see cref="T:System.ComponentModel.BindingList`1" />.</param>
-        public SortableBindingList(IList<T> list)
-            : base(list)
+        public SortableBindingList(IList<T> list) : base(list)
         {
         }
 
