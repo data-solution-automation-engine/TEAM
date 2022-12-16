23ff74c40aa1b261afc7780f0e89ea53e8286e76
Roelant Vos <roelant@ravos.nl>
2022-12-16 16:57:46 +1000

    Create TeamClassification.cs

---
+++ Team_Library/TeamClassification.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TEAM_Library
+{
+    public class TeamClassification
+    {
+        public enum TeamClassificationTypes
+        {
+            ModelLayer
+        }
+    }
+}
